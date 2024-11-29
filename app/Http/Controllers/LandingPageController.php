dda177ebfe898b88caff919e620d2417a344a317
Eric Amigo <ericamigo.dev@gmail.com>
2024-11-29 10:24:40 +0800

    student relaionship
    Signed-off-by: Eric Amigo <ericamigo.dev@gmail.com>

--- app/Http/Controllers/LandingPageController.php
+++ app/Http/Controllers/LandingPageController.php
@@ -4,32 +4,21 @@ namespace App\Http\Controllers;
 
 use App\Http\Resources\BranchResource;
 use App\Models\Branch;
+use Illuminate\Support\Facades\Auth;
 
 class LandingPageController extends Controller
 {
     public function __invoke()
     {
-        $branches = Branch::query()
-            ->withCount([
-                'campuses',
-            ])
-            ->with([
-                'campuses' => function ($campuses) {
-                    $campuses
-                        ->with([
-                            'branch',
-                            'colleges' => function ($colleges) {
-                                $colleges->orderBy('name');
-                            },
-                        ])
-                        ->orderBy('name');
-                },
-            ])
-            ->orderBy('name')
-            ->get();
-
-        // return $branches;
-
-        return BranchResource::collection($branches);
+        $student = Auth::user()->student
+            ->load([
+                'user',
+                'course',
+            ]);
+        dd(
+            $student->toArray(),
+            Auth::user()?->employee?->toArray(),
+            Auth::user()?->student?->toArray(),
+        );
     }
 }
